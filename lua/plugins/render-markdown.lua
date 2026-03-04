3797ec93b9ccecb309de01718559c83294aec986
Dennis Janssens <dennis@balgviken.com>
2026-03-05 00:25:34 +0100

    chore(render-markdown): remove comments and normalize quoting

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,21 +1,18 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- mini.icons is standard nowadays, optionally nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
     ft = { "markdown", "vimwiki" },
     config = function()
       require("render-markdown").setup({
         file_types = { "markdown", "vimwiki" },
         heading = {
-          -- Stäng av de blockiga bakgrundsfärgerna för rubriker för en renare look
           backgrounds = {},
         },
         code = {
-          -- Stäng av bakgrundsfärgen för inline-kod (t.ex. `text`)
-          highlight_inline = 'Normal',
+          highlight_inline = "Normal",
         },
       })
-      -- Registrera 'markdown' som parser för 'vimwiki'
       vim.treesitter.language.register("markdown", "vimwiki")
     end,
   },
